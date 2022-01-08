472ebb6206da6e7cf9640d947c93ca0b2b0b1e23
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-01-08 11:02:28 +0630

    main la

--- pages/albums/index.js
+++ pages/albums/index.js
@@ -1,9 +1,23 @@
 import AlbumnsLayout from "../../layout/AlbumsLayout";
 
-export default function AlbumsPage() {
+export default function AlbumsPage({ data }) {
+  console.log(data);
   return (
     <AlbumnsLayout>
       <h1>Albums pages</h1>
     </AlbumnsLayout>
   );
+}
+
+export async function getServerSideProps({query}) {
+  console.log(query);
+
+  return {
+    props: {
+      data: {
+        albums: [],
+        total: 0
+      }
+    }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import HomeLayout from "../layout/HomeLayout";
 
-export default function Home() {
+export default function Home({ home }) {
+  console.log(home);
   return (
- 
-      <HomeLayout>
-        <h1>Welcome to Next JS.</h1>
-      </HomeLayout>
- 
+    <HomeLayout>
+      <h1>Welcome to Next JS.</h1>
+    </HomeLayout>
   );
+}
+
+export async function getServerSideProps({ query }) {
+  console.log(query);
+
+  const res = await fetch(process.env.APP_URL + "/api/home");
+
+  const data = await res.json();
+
+  return {
+    props: {
+      home: data,
+    },
+  };
 }
