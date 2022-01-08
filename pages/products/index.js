8ba72965ce8050d7a24873939c37fbb0bf5f94c4
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-01-08 11:45:48 +0630

    done reviews

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,16 +1,19 @@
+import ProductsLayout from "../../components/products/ProductsLayout";
 import AlbumnsLayout from "../../layout/AlbumsLayout";
 
 export default function AlbumsPage({ data }) {
   console.log(data);
   return (
     <AlbumnsLayout>
-      <h1>Albums pages</h1>
+     
+
     </AlbumnsLayout>
   );
 }
 
 export async function getServerSideProps({query}) {
-  console.log(query);
+  
+  
 
   return {
     props: {
